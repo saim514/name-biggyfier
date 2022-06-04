0e40c29da63cf4aedb6548e3850e59241677c726
saim514 <81713694+saim514@users.noreply.github.com>
2022-06-04 17:51:20 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+difference = 0;
+left_wrist_x = 0;
+right_wrist_x = 0;
+
 function setup()
 {
     video = createCapture(VIDEO);
@@ -12,10 +16,25 @@ function setup()
 
 function modelLoaded()
 {
-    console.log("Very good website for blind people! *No offence*")
+    console.log("Very good website for blind people! *No offense*")
 }
 
 function gotPoses(results)
 {
-    
+ if(results.length > 0)
+ {
+   console.log(results);
+
+   left_wrist_x = results[0].pose.leftWrist.x;
+   right_wrist_x = results[0].pose.rightWrist.x;
+   difference = floor(left_wrist_x - right_wrist_x);
+ }   
+}
+
+function draw()
+{
+    background('#90A4AE');
+    textSize(difference);
+    fill('#84FFFF');
+    text("Saim", 50, 400);
 }
